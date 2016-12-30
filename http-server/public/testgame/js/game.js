4b07461340de62d333995abe30b6f7751c179342
arkotir <Geoffrey.hens@hotmail.be>
2016-12-30 19:29:40 +0100

    changed game

--- http-server/public/testgame/js/game.js
+++ http-server/public/testgame/js/game.js
@@ -1,22 +1,94 @@
 window.onload = function() {
 
-        //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-        //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-        //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+function preload() {
 
-        function preload () {
+    game.load.image('bullet', 'assets/sprites/shmup-bullet.png');
+    game.load.image('ship', 'img/auto.png');
 
-            game.load.image('logo', 'img/phaser.png');
+}
 
-        }
+var sprite;
+var weapon;
+var cursors;
+var fireButton;
 
-        function create () {
+function create() {
 
-            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
+    //  Creates 30 bullets, using the 'bullet' graphic
+    weapon = game.add.weapon(30, 'bullet');
 
-        }
+    //  The bullets will be automatically killed when they are 2000ms old
+    weapon.bulletKillType = Phaser.Weapon.KILL_LIFESPAN;
+    weapon.bulletLifespan = 2000;
+
+    //  The speed at which the bullet is fired
+    weapon.bulletSpeed = 600;
+
+    //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+    weapon.fireRate = 100;
+
+    //  Wrap bullets around the world bounds to the opposite side
+    weapon.bulletWorldWrap = true;
+
+    sprite = this.add.sprite(400, 300, 'ship');
+
+    sprite.anchor.set(0.5);
+
+    game.physics.arcade.enable(sprite);
+
+    sprite.body.drag.set(70);
+    sprite.body.maxVelocity.set(200);
+
+    //  Tell the Weapon to track the 'player' Sprite
+    //  With no offsets from the position
+    //  But the 'true' argument tells the weapon to track sprite rotation
+    weapon.trackSprite(sprite, 0, 0, true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+}
+
+function update() {
+
+    if (cursors.up.isDown)
+    {
+        game.physics.arcade.accelerationFromRotation(sprite.rotation, 300, sprite.body.acceleration);
+    }
+    else
+    {
+        sprite.body.acceleration.set(0);
+    }
+
+    if (cursors.left.isDown)
+    {
+        sprite.body.angularVelocity = -300;
+    }
+    else if (cursors.right.isDown)
+    {
+        sprite.body.angularVelocity = 300;
+    }
+    else
+    {
+        sprite.body.angularVelocity = 0;
+    }
+
+    if (fireButton.isDown)
+    {
+        weapon.fire();
+    }
+
+    game.world.wrap(sprite, 16);
+
+}
+
+function render() {
+
+    weapon.debug();
+
+}
 
     };
